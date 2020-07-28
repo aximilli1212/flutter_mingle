cc87fac260df4e9e55818aed654577cfdc582a38
Aximilli1212 <aximill1212@gmail.com>
2020-07-28 08:08:26 +0000

    complete appBaon headeer

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context) {
+  return AppBar(
+    title: Text('FlutterMingle', style: TextStyle(
+      color: Colors.white,
+      fontFamily: "Signatra",
+      fontSize: 5.0,
+    ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
