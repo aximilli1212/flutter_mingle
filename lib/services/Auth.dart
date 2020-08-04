eaa15b41d89933221e11272cbcc1a958e71bb4b5
Aximilli1212 <aximill1212@gmail.com>
2020-08-04 13:04:59 +0000

    update create account to save username to firestore

--- lib/services/Auth.dart
+++ lib/services/Auth.dart
@@ -1,12 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_mingle/pages/create_account.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
+
+final usersRef = Firestore.instance.collection('users');
+
 
 class AuthService {
 
   bool isAuth = false;
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final  usersRef = Firestore.instance.collection('users')  ;
   final GoogleSignIn googleSignIn = GoogleSignIn();
 
 
@@ -49,6 +55,7 @@ class AuthService {
       print('Signin Good =================>');
       isAuth = true;
       print('isAuth State $isAuth');
+      createUserInFirestore;
       return googleAuth;
     } catch (e) {
       print(e.toString());
@@ -65,6 +72,18 @@ class AuthService {
         isAuth = true;
     }else{
         isAuth = false;
+    }
+  }
+
+  createUserInFirestore(BuildContext context) async {
+    final GoogleSignInAccount user = googleSignIn.currentUser;
+    final DocumentSnapshot doc = await usersRef.document(user.id).get();
+
+    if(!doc.exists){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=> CreateAccount()));
+    }else{
+      print(user.id);
+      print("User Exits please");
     }
   }
 
