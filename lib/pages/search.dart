7edc0f55f7332fa1fb7339650865bdd60f78e659
Aximilli1212 <aximill1212@gmail.com>
2020-08-13 21:58:08 +0000

    add search users FutureBuilder

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,34 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mingle/models/user.dart';
+import 'package:flutter_mingle/widgets/progress.dart';
 import 'package:flutter_svg/svg.dart';
-
-import 'customSliver.dart';
+final usersRef = Firestore.instance.collection('users');
 
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
 }
 
-AppBar buildSearchField() {
-  return AppBar(
-    backgroundColor: Colors.white,
-    title: TextFormField(
-      decoration: InputDecoration(
-        hintText: "Search for a user ... ",
-        filled: true,
-        prefixIcon: Icon(
-          Icons.account_box,
-          size: 28.0,
-        ),
-        suffixIcon: IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: ()=>{
-            print('cleared'),
-          },
-        )
-      ),
-    ),
-  );
-}
 
 Container buildNoContent(BuildContext context){
   final Orientation orientation = MediaQuery.of(context).orientation;
@@ -56,17 +37,80 @@ Container buildNoContent(BuildContext context){
 
 
 
-
 class _SearchState extends State<Search> {
+ Future<QuerySnapshot> searchResultsFuture;
+
+ handleSearch(String query) {
+   Future<QuerySnapshot> users = usersRef.
+   where("displayName", isGreaterThanOrEqualTo: query)
+       .getDocuments();
+   setState((){
+     searchResultsFuture = users;
+   });
+ }
+
+ FutureBuilder buildSearchResults(){
+   return FutureBuilder(
+     future: searchResultsFuture,
+     builder: (context, snapshot){
+       if(!snapshot.hasData){
+         return circularProgress();
+       }
+       List<Text> searchResults = [];
+
+       snapshot.data.documents.forEach((doc){
+         User user = User.fromDocument(doc);
+         searchResults.add(Text(user.username));
+       });
+
+       return ListView(
+         children: searchResults,
+       );
+     },
+   );
+ }
+
+
+
+
+ AppBar buildSearchField() {
+   return AppBar(
+     backgroundColor: Colors.white,
+     title: TextFormField(
+       decoration: InputDecoration(
+           hintText: "Search for a user ... ",
+           filled: true,
+           prefixIcon: Icon(
+             Icons.account_box,
+             size: 28.0,
+           ),
+           suffixIcon: IconButton(
+             icon: Icon(Icons.clear),
+             onPressed: ()=>{
+               print('cleared'),
+             },
+           )
+       ),
+       onFieldSubmitted: handleSearch,
+     ),
+   );
+ }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildSearchField(),
-      body: buildNoContent(context),
+      body: searchResultsFuture == null?
+      buildNoContent(context):
+      buildSearchResults(),
     );
   }
-
 }
+
+
 
 class UserResult extends StatelessWidget {
   @override
@@ -74,3 +118,4 @@ class UserResult extends StatelessWidget {
     return Text("User Result");
   }
 }
+
